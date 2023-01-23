ec87445230e268f58dbd52f6d1960eeba72ecb23
hgatomoya <hgatomoya@LAPTOP-F90DAUS0>
2023-01-23 19:26:04 +0900

    [Finish] 4章

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "とも";
+		System.out.println(str);
+
+		str = "はが";
+		System.out.println(str);
+
+		final int LUCKY_NUMBER = 5;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
