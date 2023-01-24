e0b1ea79c7d1333969d1ca9ebca2d7aa58ce9808
hgatomoya <hgatomoya@LAPTOP-F90DAUS0>
2023-01-24 18:49:19 +0900

    [Finish] 5章

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 1;
+		double num2 = 1.1;
+		boolean num3 = true;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		
+		Float num4 = 1.5f;
+		Integer int1 = 5;
+		Character c = 'う';
+		System.out.println(num4);
+		System.out.println(int1);
+		System.out.println(c);
+		
+		int[] array = {0,1,2,3,4};
+		System.out.println(array[3]);
+	}
+}
