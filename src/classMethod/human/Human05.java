71e3c14f03b95df1011f48724fe3c32da400e52e
hgatomoya <hgatomoya@LAPTOP-F90DAUS0>
2023-01-27 03:02:45 +0900

    [Finish] 9章

---
+++ src/classMethod/human/Human05.java
@@ -0,0 +1,19 @@
+package classMethod.human; 
+
+public class Human05 {
+    private String name;
+    private int age;
+
+    public Human05() {
+        name = "ŽR“c";
+        age = 20;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/practice/Car.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Car {
+    // 燃費（Km/L）
+    private double fuelCost;
+    // 残量（L）
+    private double fuelAmount;
+
+    // コンストラクタを作成
+    public Car(double fuelCost, double fuelAmount) {
+        this.fuelCost = fuelCost;
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void move(int km) {
+        System.out.println(km + " km 走ります");
+        this.fuelAmount -= (km / fuelCost);
+    }
+
+    public double getFuelAmount() {
+        return this.fuelAmount;
+    }
+}
